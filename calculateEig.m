281bf08938a5c0b87ddccd398717e008facf9e31
KQKQ1015 <downtheline1015@gmail.com>
2024-11-10 18:31:46 +0900

    sqrt matrix is implemented

--- calculateEig.m
+++ calculateEig.m
@@ -1,17 +1,25 @@
-function eigs = calculateEig(A, maxIteration)
+function [V, D] = calculateEig(A, maxIteration)
 % QR method
+D = A;
+%[Q, R] = QRdecomposition(A);
+V = eye(size(A));
+
 for i = 1:maxIteration
-    [Q, R] = QRdecomposition(A);
-    A = R * Q;
-    if convergenceCheck(A, 1e-6)
+    [Q, R] = QRdecomposition(D);
+    D = R * Q;
+    V = V * Q;
+    
+    if convergenceCheck(D, 1e-6)
         break;
     end
 end
+
+
 % Extract eigenvalues
 n = size(A, 1);
 eigs = zeros(n, 1);
 for j = 1:n
-    eigs(j) = A(j, j);
+    eigs(j) = D(j, j);
 end
 end
 
--- test.m
+++ test.m
@@ -4,4 +4,10 @@ A = [339.1352 314.7024 -41.0372 -37.8712;
      -37.8712 -31.6588   4.8959   6.2389];
 
 
-eigA = calculateEig(A, 100);
+[P, D]  = calculateEig(A, 100);
+
+sqrtD = zeros(size(D));
+for i = 1:size(D, 1)
+    sqrtD(i, i) = sqrt(D(i, i));
+end
+sqrtA = P * sqrtD * P .';
